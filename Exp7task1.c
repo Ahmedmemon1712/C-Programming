b9c629efbc60d5362f183f8229efae21d2e392b7
Ahmedmemon1712 <ahmedmemon1723@eng.rizvi.edu.in>
2024-12-02 13:03:54 +0530

    Exp7task1.c

--- Exp7task1.c
+++ Exp7task1.c
@@ -1,7 +1,7 @@
 /* Aim: WAP to find the largest element in an array. 
-Name     : Mohammad Basharat Khan
-Roll No  : 9
-UIN      : 241A009
+Name     : Memom Ahmed
+Roll No  : 35
+UIN      : 241A043
 Division : A */
 #include<stdio.h>
 int main()
